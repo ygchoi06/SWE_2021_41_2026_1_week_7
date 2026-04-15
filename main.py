76c060807fc32b83ad5a00aa68e841f27cda6a90
ygchoi06 <ygchoi06@gmail.com>
2026-04-15 18:46:11 +0900

    Complete Week 7 Assignment

---
+++ main.py
@@ -0,0 +1,7 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
